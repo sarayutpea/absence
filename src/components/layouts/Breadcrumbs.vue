608186f474f10bbf905a5f5b78425db8afce01fe
PEA <sarayut.jansoda@hotmail.com>
2018-07-12 19:02:01 +0700

    Hello

---
+++ src/components/layouts/Breadcrumbs.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-breadcrumbs>
+      <v-icon slot="divider">chevron_right</v-icon>
+      <v-btn @click="increaseBreadcrumbs()">ทดลองเพิ่ม</v-btn>
+      <v-breadcrumbs-item v-for="item in items" :disabled="item.disabled" :key="item.text" >
+        {{ item.text }}
+      </v-breadcrumbs-item>
+      <v-btn v-if=" items.length > 1 " @click="decreaseBreadcrumbs()">ทดลองลบ</v-btn>
+    </v-breadcrumbs>
+  </div>
+</template>
+<script>
+  export default {
+    name: "Breadcrumbs",
+    data: ()=> {
+      return {
+        items: [
+          {
+            text: 'Dashboard',
+            disabled: false
+          }
+        ],
+        number: 1
+      }
+    },
+    methods: {
+      increaseBreadcrumbs(){
+        this.items.push({
+            text: 'Link ' + this.items.length,
+            disabled: true
+          });
+      },
+      decreaseBreadcrumbs(){
+        this.items.splice(-1, 1);
+      }
+    }
+  }
+</script>
+<style scope>
+
+</style>
--- src/components/layouts/Master.vue
+++ src/components/layouts/Master.vue
@@ -1,60 +1,56 @@
 <template>
     <div class="master">
         <v-app id="inspire">
-        <v-navigation-drawer :clipped="$vuetify.breakpoint.lgAndUp" v-model="drawer" fixed app>
         <!-- menu position -->
-        <v-list dense>
-            <template v-for="item in items">
-            <v-layout v-if="item.heading" :key="item.heading" row align-center>
-                <v-flex xs6>
-                    <v-subheader v-if="item.heading">
-                        {{ item.heading }}
-                    </v-subheader>
-                </v-flex>
-                <v-flex xs6 class="text-xs-center">
-                    <a href="#!" class="body-2 black--text">EDIT</a>
-                </v-flex>
-            </v-layout>
-            <v-list-group v-else-if="item.children" v-model="item.model" :key="item.text" :prepend-icon="item.model ? item.icon : item['icon-alt']" append-icon="">
-                <v-list-tile slot="activator">
-                    <v-list-tile-content>
+        <v-navigation-drawer :clipped="$vuetify.breakpoint.lgAndUp" v-model="drawer" fixed app>
+            <v-list dense>
+                <template v-for="item in items">
+                    <v-list-group v-if="item.children" v-model="item.model" :key="item.text" :prepend-icon="item.model ? item.icon : item['icon-alt']" append-icon="">
+                        <v-list-tile slot="activator">
+                            <v-list-tile-content>
+                                <v-list-tile-title>
+                                    {{ item.text }}
+                                </v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                        <v-list-tile v-for="(child, i) in item.children" :key="i" @click="" >
+                            <v-list-tile-action v-if="child.icon">
+                                <v-icon>{{ child.icon }}</v-icon>
+                            </v-list-tile-action>
+                            <v-list-tile-content>
+                                <v-list-tile-title>
+                                {{ child.text }}
+                                </v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                    </v-list-group>
+                    <v-list-tile v-else :key="item.text" @click="">
+                        <v-list-tile-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
                         <v-list-tile-title>
-                        {{ item.text }}
+                            {{ item.text }}
                         </v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-                <v-list-tile v-for="(child, i) in item.children" :key="i" @click="" >
-                    <v-list-tile-action v-if="child.icon">
-                        <v-icon>{{ child.icon }}</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                        <v-list-tile-title>
-                        {{ child.text }}
-                        </v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-            </v-list-group>
-            <v-list-tile v-else :key="item.text" @click="">
-                <v-list-tile-action>
-                <v-icon>{{ item.icon }}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                <v-list-tile-title>
-                    {{ item.text }}
-                </v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
-            </template>
-        </v-list>
-        <!-- end menu position -->
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </template>
+            </v-list>
         </v-navigation-drawer>
+        <!-- end menu position -->
+
         <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" color="blue darken-3" dark app fixed>
             <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
                 <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
                 <span class="hidden-sm-and-down">Absence</span>
             </v-toolbar-title>
-            Title Position
-            <v-spacer></v-spacer>
+                <v-btn icon >
+                    <v-icon>keyboard_arrow_left</v-icon>
+                </v-btn>
+                <breadcrumbs />
+            <v-spacer>
+
+            </v-spacer>
                     
             <v-btn icon large>
                 <v-avatar size="32px" tile>
@@ -65,6 +61,8 @@
         <v-content>
             <v-container fluid fill-width>
                 Content position
+
+                {{ breadcrumbs }}
                 <Login />
             </v-container>
         </v-content>
@@ -75,18 +73,20 @@
 </template>
 <script>
 import Login from "../../components/auth/Login.vue";
+import Breadcrumbs from '../../components/layouts/Breadcrumbs.vue';
 
 export default {
   name: "Master",
   components: {
-    Login
+    Login,
+    Breadcrumbs
   },
-  data: () => ({
+  data: ()=>({
     dialog: false,
     drawer: null,
     items: [
-      { icon: "computer", text: "Dashboard" },
-      { icon: "history", text: "Salary" },
+      { icon: "computer", text: "Dashboard", link: "#" },
+      { icon: "history", text: "Salary", link: "#" },
     //   { icon: "content_copy", text: "Duplicates" },
     //   {
     //     icon: "keyboard_arrow_up",
@@ -95,25 +95,22 @@ export default {
     //     model: true,
     //     children: [{ icon: "add", text: "Create label" }]
     //   },
-    //   {
-    //     icon: "keyboard_arrow_up",
-    //     "icon-alt": "keyboard_arrow_down",
-    //     text: "More",
-    //     model: false,
-    //     children: [
-    //       { text: "Import" },
-    //       { text: "Export" },
-    //       { text: "Print" },
-    //       { text: "Undo changes" },
-    //       { text: "Other contacts" }
-    //     ]
-    //   },
+      {
+        icon: "keyboard_arrow_up", 
+        'icon-alt' : "keyboard_arrow_down",
+        text: "More",
+        model: false,
+        children: [
+          { text: "Logout" },
+        ]
+      }
     //   { icon: "settings", text: "Settings" },
     //   { icon: "chat_bubble", text: "Send feedback" },
     //   { icon: "help", text: "Help" },
     //   { icon: "phonelink", text: "App downloads" },
     //   { icon: "keyboard", text: "Go to the old version" }
-    ]
+    ],
+    breadcrumbs: Breadcrumbs.items
   }),
   props: {
     source: String
